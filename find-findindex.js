e68b8f6f0d5ac240519eacc0699c3d14cdd0256d
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-10 07:44:54 -0500

    Complete findUserByUsername exercise

--- find-findindex.js
+++ find-findindex.js
@@ -2,17 +2,18 @@
 Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
 
 const users = [
-  {username: 'mlewis'},
-  {username: 'akagen'},
-  {username: 'msmith'}
+  { username: "mlewis" },
+  { username: "akagen" },
+  { username: "msmith" },
 ];
 
 findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
-
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find((user) => user.username === username);
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
